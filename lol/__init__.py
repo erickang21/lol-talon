056a9134a4f405e516f5976e9929f7134e2aac92
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-03-07 15:51:38 -0500

    added get_champion func

--- lol/__init__.py
+++ lol/__init__.py
@@ -2,6 +2,10 @@ import aiohttp
 
 
 class LolError(Exception):
+	'''
+	Raised when the Riot API returns a status code other than 200 (success).
+	This error does nothing by itself, you will have to check for the exception and handle it yourself.
+	'''
 	pass
 
 
@@ -23,16 +27,34 @@ class Client:
 
     async def get_summoner(self, region=None, query):
     	'''
-    	Basic request that gets an LoL summoner's profile, by their name.
+    	Request that gets an LoL summoner's profile, by their name.
+
     	---Arguments---
-    	region (str): The region to execute this request on. 
+    	region (Optional, str): The region to execute this request on. 
     	All possible regions for this argument include: ru, kr, br1, oc1, jp1, na1, eun1, euw1, tr1, la1, la2.
-    	Defaults to North America if left as None.
-    	query (str): The Summoner's username. 
+    	Defaults to North America if region is None.
+
+    	query (str): The Summoner's username. Mandatory for this request.
     	'''
     	if region is None:
     		region = 'na1'
     	return await self._get(f"summoner/v3/summoners/by-name/{query}", region)
+
+
+    async def get_champion(self, region=None, query):
+    	'''Request that gets LoL champion information for a specific champion, by the Champion ID.
+    	---Arguments---
+    	region (Optional, str): The region to execute this request on. 
+    	All possible regions for this argument include: ru, kr, br1, oc1, jp1, na1, eun1, euw1, tr1, la1, la2.
+    	Defaults to North America if region is None.
+		
+		query (str): The ID of the champion that is being requested. Mandatory for this request.
+    	'''
+    	if region is None:
+    		region = 'na1'
+    	return await self._get(f"/lol/static-data/v3/champions/{query}", region)
+
+
 
 
 
