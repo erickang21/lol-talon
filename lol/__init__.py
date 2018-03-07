dae5ebfe19259a48e7223ceeea89e454c499aaca
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-03-06 20:33:34 -0500

    rise 'n shine! ready to ROLL!

--- lol/__init__.py
+++ lol/__init__.py
@@ -1,1 +1,39 @@
+import aiohttp
+
+
+class LolError(Exception):
+	pass
+
+
+class Client:
+	def __init__(self, token, session=None):
+        self.token = token
+        self.query_string = {"api_key": token}
+        self.base_url = ".api.riotgames.com/lol/"
+        self.session = aiohttp.ClientSession() if session is None else session
+
+
+    async def _get(self, endpoint, query, region):
+    	async with self.session.get(f"https://{region}{self.base_url}{endpoint}{query}", params=self.query_string) as resp:
+    		if resp.status != 200:
+    			raise LolError(f"Riot API returned a non-200 code. Details: {resp.status}")
+    		resp = await resp.json()
+    		return resp['data']
+
+
+    async def get_summoner(self, region=None, query):
+    	'''
+    	Basic request that gets an LoL summoner's profile, by their name.
+    	---Arguments---
+    	region (str): The region to execute this request on. 
+    	All possible regions for this argument include: ru, kr, br1, oc1, jp1, na1, eun1, euw1, tr1, la1, la2.
+    	Defaults to North America if left as None.
+    	query (str): The Summoner's username. 
+    	'''
+    	if region is None:
+    		region = 'na1'
+    	return await self._get(f"summoner/v3/summoners/by-name/{query}", region)
+
+
+
 
--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+import setuptools
 
+description = "Asynchronous League Of Legends API Wrapper"
+long_description = open("README.md").read()
+version="1.0.0"
+
+packages = ['lolbanana']
+
+setuptools.setup(
+    name='League of Bananas',
+    version=version,
+    description=description,
+    long_description=long_description,
+    url='https://github.com/bananaboy21/league-of-bananas',
+    author='dat banana boi',
+    author_email='kang.eric.hi@gmail.com',
+    license='MIT',
+    packages=packages,
+    include_package_data=True,
+    install_requires=['aiohttp>=2.0.0']
+)
